5497149254741bf63dde5739ec9ac58fde568739
Michael Stedman <mikeybones@rocketmail.com>
2019-06-14 13:51:02 -0600

    fixing stuff

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
-import BlogProvider from "./components/BlogProvider"
+// import BlogProvider from "./components/BlogProvider"
 import App from "./components/App"
-import styles from "./css/index.css"
+// import styles from "./css/index.css"
 
 ReactDOM.render(
     <BrowserRouter>
