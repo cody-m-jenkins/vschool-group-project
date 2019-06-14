10e5d2710bb002292a61ac5a83f0f22ab449817c
Michael Stedman <mikeybones@rocketmail.com>
2019-06-14 14:00:08 -0600

    changes

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import BrowserRouter from "react-router-dom"
 import BlogProvider from "./BlogProvider"
+import App from './components/App'
 
 ReactDOM.render(
     <BrowserRouter>
