ddb25689e8f142781278942af9a3cb048f394c8b
Cody Jenkins <cody.m.jenkins@gmail.com>
2019-06-18 11:42:35 -0600

    changed convention of input classname

--- client/src/components/PostPage.js
+++ client/src/components/PostPage.js
@@ -6,31 +6,31 @@ const PostPage = () => {
         <div className='post-page-container'>
             <div className='post-page-container-title'>
                 <h3>Title of your blog post (recommended 100 characters or less): </h3>
-                <input className='post-page-container-title-input' required type='string' name='blogTitle'/>
+                <input className='post-page-input-title' required type='string' name='blogTitle'/>
             </div>
             <div className='post-page-container-description'>
                 <h3>Description of the blog post (recommended 300 characters or less): </h3>
-                <input className='post-page-container-description-input' required type='string' name='blogDescription'/>
+                <input className='post-page-input-description' required type='string' name='blogDescription'/>
             </div>
             <div className='post-page-container-author'>
                 <h3>Names of the author(s) who have contributed to the blog post:</h3>
-                <input className='post-page-container-author-input' required type='string' name='blogAuthor'/>
+                <input className='post-page-input-author' required type='string' name='blogAuthor'/>
             </div>
             <div className='post-page-container-body'>
                 <h3>The body of your blog post:</h3>
-                <input className='post-page-container-body-input' required type='string' name='blogBody'/>
+                <input className='post-page-input-body' required type='string' name='blogBody'/>
             </div>
             <div className='post-page-container-blogImgUrl'>
                 <h3>URL of the thumbnail image for your post (optional):</h3>
-                <input className='post-page-container-blogImgUrl-input' type='string' name='blogImgUrl'/>
+                <input className='post-page-input-blogImgUrl' type='string' name='blogImgUrl'/>
             </div>
             <div className='post-page-container-blogTags'>
                 <h3>Tags for your post (optional):</h3>
-                <input className='post-page-container-blogTags-input' type='string' name='blogTags'/>
+                <input className='post-page-input-blogTags' type='string' name='blogTags'/>
             </div>
             <div className='post-page-container-blogDate'>
                 <h3>Date of the post:</h3>
-                <input className='post-page-container-blogDate-input' type='string' name='blogDate'/>
+                <input className='post-page-input-blogDate' type='string' name='blogDate'/>
             </div>
         </div>
     )
