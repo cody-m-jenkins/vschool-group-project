c5a1923a07c780da3dfee90710c34c5640f03b11
Michael Stedman <mikeybones@rocketmail.com>
2019-06-18 12:25:28 -0600

    Updated Navbar and About components
    Updated Navbar and About components.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,11 +1,40 @@
-import React from "react"
+import React from 'react'
 import '../css/about.css'
 
 const About = () => {
     return(
         <div className='about-container'>
-            <h1>About Me</h1>
-        </div>
+            <h1>Strauss "First-Ascent" Van Wagenen</h1>
+            <div className='about-section left'>
+                <div className='section-image'>
+                    <img src='https://www.mayorsfundforlondon.org.uk/wp-content/uploads/2016/06/img-placeholder-square-300x300.jpg' alt='placeholder' />
+                </div>
+                <div className='section-info'>
+                    <h3>Start of "Little Straussy"</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et assumenda minus dolores error illum reiciendis quis, vitae exercitationem, earum nisi, aut nobis architecto quos sint repellendus esse. Minima, quod deleniti?</p>
+                </div>
+            </div>
+
+        {/* <div className='about-section right'>
+            <div className='section-image'>
+                    <img src='https://www.mayorsfundforlondon.org.uk/wp-content/uploads/2016/06/img-placeholder-square-300x300.jpg' alt='placeholder' />
+                </div>
+                <div className='section-info'>
+                    <h3>Start of "Little Straussy"</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et assumenda minus dolores error illum reiciendis quis, vitae exercitationem, earum nisi, aut nobis architecto quos sint repellendus esse. Minima, quod deleniti?</p>
+                </div>
+            </div> */}
+
+        {/* <div className='about-section left'>
+            <div className='section-image'>
+                    <img src='https://www.mayorsfundforlondon.org.uk/wp-content/uploads/2016/06/img-placeholder-square-300x300.jpg' alt='placeholder' />
+                </div>
+                <div className='section-info'>
+                    <h3>Start of "Little Straussy"</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et assumenda minus dolores error illum reiciendis quis, vitae exercitationem, earum nisi, aut nobis architecto quos sint repellendus esse. Minima, quod deleniti?</p>
+                </div>
+        </div> */}
+    </div>
     )
 }
 
