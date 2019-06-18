3f06c1831690e759bd79330a50a2095208aa9916
Michael Stedman <mikeybones@rocketmail.com>
2019-06-18 14:45:34 -0600

    Commit
    Commit before class ends.

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -6,8 +6,8 @@ const About = () => {
         <div className='about-container'>
             <h1>Strauss "First-Ascent" Van Wagenen</h1>
             <div className='about-section left'>
-                <div className='section-image'>
-                    <img src='https://www.mayorsfundforlondon.org.uk/wp-content/uploads/2016/06/img-placeholder-square-300x300.jpg' alt='placeholder' />
+                <div className='section-image left=='>
+                    {/* <img src= 'https://imgix.bustle.com/2017/4/27/c5576501-1a7a-43b8-b5e4-9f0aed1083e0.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70' alt='placeholder' /> */}
                 </div>
                 <div className='section-info'>
                     <h3>Start of "Little Straussy"</h3>
