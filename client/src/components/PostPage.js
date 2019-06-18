05c08ab9ece2898f5536b0ab68cf9552a0dee8f3
Cody Jenkins <cody.m.jenkins@gmail.com>
2019-06-18 10:51:04 -0600

    made basic inputs for postpage

--- client/src/components/PostPage.js
+++ client/src/components/PostPage.js
@@ -4,7 +4,34 @@ import '../css/post-page.css'
 const PostPage = () => {
     return (
         <div className='post-page-container'>
-            <h2>PostPage component</h2>
+            <div className='post-page-container-title'>
+                <h3>Title of your blog post (recommended 100 characters or less): </h3>
+                <input required type='string' name='blogTitle'/>
+            </div>
+            <div className='post-page-container-description'>
+                <h3>Description of the blog post (recommended 300 characters or less): </h3>
+                <input required type='string' name='blogDescription'/>
+            </div>
+            <div className='post-page-container-author'>
+                <h3>Names of the author(s) who have contributed to the blog post:</h3>
+                <input required type='string' name='blogAuthor'/>
+            </div>
+            <div className='post-page-container-body'>
+                <h3>The body of your blog post:</h3>
+                <input required type='string' name='blogBody'/>
+            </div>
+            <div className='post-page-container-blogImgUrl'>
+                <h3>URL of the thumbnail image for your post (optional):</h3>
+                <input type='string' name='blogImgUrl'/>
+            </div>
+            <div className='post-page-container-blogTags'>
+                <h3>Tags for your post (optional):</h3>
+                <input type='string' name='blogTags'/>
+            </div>
+            <div className='post-page-container-blogDate'>
+                <h3>Date of the post:</h3>
+                <input type='string' name='blogDate'/>
+            </div>
         </div>
     )
 }
