58e00534a96676f91f5c5edb62a2589dc5bdfb62
Michael Stedman <mikeybones@rocketmail.com>
2019-06-20 05:38:43 -0600

    Update PostPage
    Changed Date to String (But might want to re-add the Date type for the presentation unless there is some functionality that won't be able to happen without it.

--- client/src/components/PostPage.js
+++ client/src/components/PostPage.js
@@ -45,89 +45,95 @@ class PostPage extends Component {
         return (
             <div className='post-page-container'>
                 <form onSubmit={this.onSubmit}>
-                    <div className='input-container'>
-                        <label>Post Title:</label>
-                        <input 
-                        className='input-title' 
-                        required type='string' 
-                        name='title' 
-                        value={title} 
-                        onChange={this.onChange}
-                        placeholder='100 Characters' />
+                    <div className='split'>
+                        <div className='left-inputs'>
+                            <div className='input-container'>
+                                <label>Title:</label>
+                                <input 
+                                className='input-title title' 
+                                required type='string' 
+                                name='title' 
+                                value={title} 
+                                onChange={this.onChange}
+                                placeholder='100 Characters' />
+                            </div>
+
+                            <div className='input-container'>
+                                <label>Date:</label>
+                                <input 
+                                className='input-blogDate' 
+                                type='string' 
+                                name='date' 
+                                value={date}
+                                onChange={this.onChange}
+                                placeholder='Date' />
+                            </div>
+                        </div>
+
+                        <div className='right-inputs'>
+                            <div className='input-container'>
+                                <label>Author:</label>
+                                <input 
+                                className='input-author' 
+                                required type='string' 
+                                name='author' 
+                                value={author}
+                                onChange={this.onChange}
+                                placeholder='Post Author' />
+                            </div>
+
+                            <div className='input-container'>
+                                <label>Tags:</label>
+                                <input 
+                                className='input-blogTags' 
+                                type='string' 
+                                name='tags' 
+                                value={tags}
+                                onChange={this.onChange}
+                                placeholder='Tags' />
+                            </div>
+                        </div>
                     </div>
 
-                    <div className='input-container'>
-                        <label>Desc:</label>
+                    <div className='center-inputs'>
 
-                        <textarea 
-                        rows='4' cols='50' 
-                        className='input-description' 
-                        required type='string' 
-                        name='description'
-                        value={description}
-                        onChange={this.onChange}
-                        placeholder='Post Description'></textarea>
+                        <div className='input-container'>
+                            <label>Image URL:</label>
+                            <input 
+                            className='input-blogImgUrl'
+                            size='50' 
+                            type='string' 
+                            name='imgUrl' 
+                            value={imgUrl}
+                            onChange={this.onChange}
+                            placeholder='Image URL' />
+                        </div>
 
-                    </div>
-
-                    <div className='input-container'>
-                        <label>Author:</label>
-                        <input 
-                        className='input-author' 
-                        required type='string' 
-                        name='author' 
-                        value={author}
-                        onChange={this.onChange}
-                        placeholder='Post Author' />
-                    </div>
-
-                    <div className='input-container'>
-                        <label>Date:</label>
-                        <input 
-                        className='input-blogDate' 
-                        type='date' 
-                        name='date' 
-                        value={date}
-                        onChange={this.onChange}
-                        placeholder='Date' />
-                    </div>
-                    
-                    <div className='input-container'>
-                        <label>Tags:</label>
-                        <input 
-                        className='input-blogTags' 
-                        type='string' 
-                        name='tags' 
-                        value={tags}
-                        onChange={this.onChange}
-                        placeholder='Tags' />
-                    </div>
-
-                    <div className='input-container'>
-                        <label>Image URL:</label>
-                        <input 
-                        className='input-blogImgUrl' 
-                        type='string' 
-                        name='imgUrl' 
-                        value={imgUrl}
-                        onChange={this.onChange}
-                        placeholder='Image URL' />
-                    </div>
-
-                    <div className='input-container'>
-                        <label>Body:</label>
-                        <textarea 
-                        rows="4" cols="50" 
-                        className='input-body' 
-                        required type='string' 
-                        name='blogBody'
-                        value={blogBody}
-                        onChange={this.onChange}
-                        ></textarea>
+                        <div className='input-container'>
+                            <label>Desc:</label>
+                            <textarea 
+                            rows='1' cols='50' 
+                            className='input-description' 
+                            required type='string' 
+                            name='description'
+                            value={description}
+                            onChange={this.onChange}
+                            placeholder='Post Description'>Enter Description Here</textarea>
+                        </div>
+                        <div className='input-container'>
+                            <label>Body:</label>
+                            <textarea 
+                            rows="10" cols="50" 
+                            className='input-body' 
+                            required type='string' 
+                            name='blogBody'
+                            value={blogBody}
+                            onChange={this.onChange}
+                            >Enter Body Content Here</textarea>
+                        </div>
                     </div>
                     <button type='submit'>Submit</button>
                 </form>
-
             </div>
         )    
     }
