354dcbfd43709abbadd6963333496ee7fd49f5b4
Strauss Van Wagenen <strauss.vanwagenen@gmail.com>
2019-06-20 12:17:42 -0600

    here we go

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,8 +5,8 @@ import '../css/navbar.css'
 const Navbar = () => {
     return (
         <div className='navbar-container'>
-            <Link to='/'>Blog</Link>
-            <Link to='/about'>About</Link>
+            <Link to='/' ><img id="houseLogo" src="https://cdn.hipwallpaper.com/i/18/79/rFtbm9.png" alt="house"/></Link>
+            <Link to='/about' id='about-link'>About</Link>
             <Link to='/postPage'>Post</Link>
         </div>
     )
